49e6e23967e2a4d82a9d7aa7b141fae6d3eb2888
凤之兮原 <1577310596@qq.com>
2022-10-17 23:53:57 +0800

    feat: add axios interceptors

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { getToken } from './token'
 
 const http = axios.create({
   // baseURL: '',
@@ -7,6 +8,10 @@ const http = axios.create({
 
 
 http.interceptors.request.use((config) => {
+  const token = getToken()
+  if (token) {
+    config.headers!.Authorization = `Bearer ${token}`
+  }
   return config
 }, (error) => {
   return Promise.reject(error)
@@ -16,6 +21,12 @@ http.interceptors.request.use((config) => {
 http.interceptors.response.use((response) => {
   return response
 }, (error) => {
+  // 超出 2xx 范围的状态码都会触发该函数
+  // 401 未授权
+  if (error.response!.status === 401) {
+    // 跳转到登录页
+    // history.push('/login')
+  }
   return Promise.reject(error)
 })
 
