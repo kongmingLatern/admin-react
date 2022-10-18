ee415d8ade29d2c985cc4993f614eb405a80f048
凤之兮原 <1577310596@qq.com>
2022-10-18 23:17:08 +0800

    remove: remove css

--- src/components/UI.tsx
+++ src/components/UI.tsx
@@ -30,8 +30,8 @@ export default function UI() {
       </Sider>
       <Layout>
         <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
-        <Content style={{ margin: '24px 16px 0' }}>
-          <div className="site-layout-background" style={{ padding: 24, minHeight: 400 }}>
+        <Content>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 350 }}>
             <Outlet />
           </div>
         </Content>
