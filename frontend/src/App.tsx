80b578ee454cc4c1f72bb105e708bfb9787b6df4
凤之兮原 <1577310596@qq.com>
2022-10-20 19:51:57 +0800

    update: update components path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from 'react-router-dom'
 import UI from './components/UI'
-import Goods from './pages/Goods'
-import User from './pages/User'
-import Order from './pages/Order'
+import Goods from './views/back/Goods'
 import Header from './components/Header'
+import Order from './views/back/Order'
+import User from './views/back/User'
 
 export default function App() {
   return (
