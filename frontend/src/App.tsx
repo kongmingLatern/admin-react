0492d25c6b727e26187c1b5282ea2a9f223e0beb
凤之兮原 <1577310596@qq.com>
2022-10-20 20:28:51 +0800

    change router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import UI from './components/UI'
 import Goods from './views/back/Goods'
-import Header from './components/Header'
 import Order from './views/back/Order'
 import User from './views/back/User'
 
@@ -9,16 +8,17 @@ export default function App() {
   return (
     <>
       <Routes>
+        <Route path="/">
+          <Route path="/" element={<div>index</div>}></Route>
+        </Route>
+
         <Route path="/admin" element={<UI />}>
           <Route path="user" element={<User />}></Route>
           <Route path="goods" element={<Goods />}></Route>
           <Route path="order" element={<Order />}></Route>
         </Route>
 
-        <Route path="/">
-          <Route path="/" element={<div>index</div>}></Route>
-          <Route path="*" element={<div>404</div>}></Route>
-        </Route>
+        <Route path="*" element={<div>404</div>}></Route>
       </Routes>
     </>
   )
