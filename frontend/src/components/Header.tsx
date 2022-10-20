4d5f5bfef50087845355e1df7164665ad4d89b69
凤之兮原 <1577310596@qq.com>
2022-10-20 22:26:25 +0800

    enhance: enhance Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,14 @@
 import { Button } from 'antd'
-export default function Header({ title, exit }) {
+export default function Header({ title, text, isLogin = false, click = () => {} }) {
   return (
     <header className="font-semibold  bg-[#001529] relative ">
       <h1 className="text-[#fff] text-lg  p-6">{title}</h1>
 
       <div className="absolute top-6 right-6">
-        <span className="text-white mr-3">管理员: Username</span>
-        <Button type="primary">{exit}</Button>
+        <span className="text-white mr-3">{isLogin && '管理员: Username'}</span>
+        <Button type="primary" onClick={click}>
+          {text}
+        </Button>
       </div>
     </header>
   )
