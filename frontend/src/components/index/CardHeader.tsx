88d4a23cfdc61db9c606fc35b1eced3bf4b9d9a8
凤之兮原 <1577310596@qq.com>
2022-10-20 22:27:03 +0800

    feat: add CardHeader.tsx

---
+++ frontend/src/components/index/CardHeader.tsx
@@ -0,0 +1,8 @@
+export default function CardHeader({ name, price }) {
+  return (
+    <header className="flex justify-around items-center  pl-5 pr-5">
+      <div className="w-50 text-ellipsis mr-5 overflow-hidden">{name}</div>
+      <div className="text-red-500 font-bold text-lg">￥{price}</div>
+    </header>
+  )
+}
