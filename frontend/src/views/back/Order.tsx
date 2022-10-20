3c28154039afe94e91a0f6e4b28eeeff05327185
凤之兮原 <1577310596@qq.com>
2022-10-20 20:18:20 +0800

    feat: add Order.tsx

--- frontend/src/views/back/Order.tsx
+++ frontend/src/views/back/Order.tsx
@@ -1,3 +1,59 @@
-export default function Order() {
-  return <div>Order</div>
-}
+import { Space, Table } from 'antd'
+import type { ColumnsType } from 'antd/es/table'
+import React from 'react'
+import { OrderType } from '../../type'
+
+const columns: ColumnsType<OrderType> = [
+  {
+    title: 'oid',
+    dataIndex: 'oid',
+    key: 'oid',
+  },
+  {
+    title: 'gid',
+    dataIndex: 'gid',
+    key: 'gid',
+  },
+  {
+    title: '订单时间',
+    dataIndex: 'otime',
+    key: 'otime',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+]
+
+const data: OrderType[] = [
+  {
+    oid: '1212',
+    gid: '1212',
+    uid: '111',
+    otime: 2021,
+    isFinish: 0,
+  },
+  {
+    oid: '1212',
+    gid: '1212',
+    uid: '111',
+    otime: 2021,
+    isFinish: 0,
+  },
+  {
+    oid: '1212',
+    gid: '1212',
+    uid: '111',
+    otime: 20212121,
+    isFinish: 1,
+  },
+]
+
+const App: React.FC = () => <Table columns={columns} dataSource={data} />
+
+export default App
