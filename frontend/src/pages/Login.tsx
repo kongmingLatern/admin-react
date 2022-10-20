370374dcb6a3e72ab3018c0ddfc5c9bb63499392
凤之兮原 <1577310596@qq.com>
2022-10-20 23:38:01 +0800

    feat: add LoginBox

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,16 @@
 import { useNavigate } from 'react-router-dom'
 import Header from '../components/Header'
+import LoginBox from '../views/login/loginBox'
 
 export default function Login() {
   const navigate = useNavigate()
   return (
     <>
-      <Header title="登录" text="注册" click={() => navigate('/regist')} />
+      {/* 头部 */}
+      <Header title="登录" text="去注册" click={() => navigate('/regist')} />
+
+      {/* 登录框 */}
+      <LoginBox />
     </>
   )
 }
