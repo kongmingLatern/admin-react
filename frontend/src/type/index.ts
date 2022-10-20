5619a8408bc935fb1899c9066e7d1e81652e625c
凤之兮原 <1577310596@qq.com>
2022-10-20 20:18:00 +0800

    feat: add UserTable.tsx

---
+++ frontend/src/type/index.ts
@@ -0,0 +1,26 @@
+
+export interface UserType {
+  uid: string
+  key: string
+  name: string
+  tel: string
+  address: string
+  balance: string
+}
+export interface GoodsType {
+  gid: string
+  key?: string
+  gname: string
+  type: string
+  imgs: string[]
+  desc: string
+  goodcount: number
+  price: number
+}
+export interface OrderType {
+  oid: string
+  uid: string
+  gid: string
+  otime: number
+  isFinish: number
+}
