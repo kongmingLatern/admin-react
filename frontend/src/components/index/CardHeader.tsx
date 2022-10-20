6f35b2dca7d32682e6dd5de0c2b265448cabe106
凤之兮原 <1577310596@qq.com>
2022-10-20 23:37:42 +0800

    change: change fontSize

--- frontend/src/components/index/CardHeader.tsx
+++ frontend/src/components/index/CardHeader.tsx
@@ -2,7 +2,7 @@ export default function CardHeader({ name, price }) {
   return (
     <header className="flex justify-around items-center  pl-5 pr-5">
       <div className="w-50 text-ellipsis mr-5 overflow-hidden">{name}</div>
-      <div className="text-red-500 font-bold text-lg">￥{price}</div>
+      <div className="text-red-500 font-bold text-3xl">￥{price}</div>
     </header>
   )
 }
