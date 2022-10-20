7a8faea1561c63f9fc5465dd9481d904ce1fbd8a
凤之兮原 <1577310596@qq.com>
2022-10-20 20:29:03 +0800

    feat: add props

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,12 @@
 import { Button } from 'antd'
-export default function Header() {
+export default function Header({ title, exit }) {
   return (
     <header className="font-semibold  bg-[#001529] relative ">
-      <h1 className="text-[#fff] text-lg  p-6">后台管理系统</h1>
+      <h1 className="text-[#fff] text-lg  p-6">{title}</h1>
 
       <div className="absolute top-6 right-6">
         <span className="text-white mr-3">管理员: Username</span>
-        <Button type="primary">退出登录</Button>
+        <Button type="primary">{exit}</Button>
       </div>
     </header>
   )
