78f8c47515a1fb985d279858ae7ae7004a2f37ec
凤之兮原 <1577310596@qq.com>
2022-10-20 22:27:51 +0800

    feat: add navigate

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,28 @@
+import { useNavigate } from 'react-router-dom'
+import Header from '../components/Header'
+import Carousel from '../views/index/Carousel'
+import Content from '../views/index/Content'
+import Footer from '../views/index/Footer'
+
 export default function Index() {
-  return <>Index</>
+  const navigate = useNavigate()
+  return (
+    <>
+      {/* 头部 */}
+      <Header
+        title="一枝花"
+        text="登录"
+        click={() => {
+          navigate('/login')
+        }}
+      />
+      {/* 轮播图 */}
+      <Carousel />
+
+      {/* 今日推荐： 商品展示 */}
+      <Content />
+
+      <Footer />
+    </>
+  )
 }
