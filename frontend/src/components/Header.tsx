3a4d5ff9c55c32b03f746ec3bd34b81bac388558
凤之兮原 <1577310596@qq.com>
2022-10-20 23:37:52 +0800

    change: add w-100%

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { Button } from 'antd'
 export default function Header({ title, text, isLogin = false, click = () => {} }) {
   return (
-    <header className="font-semibold  bg-[#001529] relative ">
+    <header className="font-semibold  bg-[#001529] relative w-[100%]">
       <h1 className="text-[#fff] text-lg  p-6">{title}</h1>
 
       <div className="absolute top-6 right-6">
