c727a5cf32745fa8e1dbbf4d7923827507a306fd
凤之兮原 <1577310596@qq.com>
2022-10-20 22:26:13 +0800

    feat: finish Footer.tsx

---
+++ frontend/src/views/index/Footer.tsx
@@ -0,0 +1,11 @@
+import { Space } from 'antd'
+export default function Footer() {
+  return (
+    <div className="bg-[#001529] text-[#fff] text-center py-4">
+      <Space direction="vertical">
+        <span>网上购物后台</span>
+        <p>&copy;&nbsp;0922202 一枝花</p>
+      </Space>
+    </div>
+  )
+}
