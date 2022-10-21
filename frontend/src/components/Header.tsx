6147f7fc8da39abb74279a121e56b6696cc8c3b4
凤之兮原 <1577310596@qq.com>
2022-10-21 21:04:41 +0800

    enhance: enhance Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,20 @@
 import { Button } from 'antd'
+import { useNavigate } from 'react-router-dom'
+
 export default function Header({ title, text, isLogin = false, click = () => {} }) {
+  const navigate = useNavigate()
   return (
     <header className="font-semibold  bg-[#001529] relative w-[100%]">
       <h1 className="text-[#fff] text-lg  p-6">{title}</h1>
 
       <div className="absolute top-6 right-6">
-        <span className="text-white mr-3">{isLogin && '管理员: Username'}</span>
+        <span className="text-white mr-3">
+          {isLogin && (
+            <Button type="primary" onClick={() => navigate('/personal')}>
+              我的
+            </Button>
+          )}
+        </span>
         <Button type="primary" onClick={click}>
           {text}
         </Button>
