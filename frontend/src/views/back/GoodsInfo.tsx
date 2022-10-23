a32971f9cd2c70f04c37db00a194de740b8992ca
凤之兮原 <1577310596@qq.com>
2022-10-23 23:00:26 +0800

    change css

--- frontend/src/views/back/GoodsInfo.tsx
+++ frontend/src/views/back/GoodsInfo.tsx
@@ -150,7 +150,7 @@ const App: React.FC = () => {
               listType="picture"
               withCredentials={true}
             >
-              <Button icon={<UploadOutlined />} className="text-inherit">
+              <Button type='ghost' icon={<UploadOutlined />} className="text-white" >
                 点击上传
               </Button>
             </Upload>
